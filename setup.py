425b35010fb190f60e0f0eb35099c506a4bcf182
dylandavi06 <dylan.automation.ia@gmail.com>
2026-07-03 12:35:01 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="figuritas-mundial-2026",
+    version="1.0.0",
+    description="Generador profesional de figuritas coleccionables",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    include_package_data=True,
+    zip_safe=False,
+)
